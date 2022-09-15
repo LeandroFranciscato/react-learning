18503774f410c68c493997da1e8d5a9152cb3e6f
Leandro Franciscato <leandro.franciscato@clevertech.biz>
2022-09-15 00:22:21 -0300

    finshing initial tests

--- src/components/ProductTableFooter.js
+++ src/components/ProductTableFooter.js
@@ -1,4 +1,4 @@
-import { TablePagination } from "@mui/material"
+import { TablePagination, TableRow } from "@mui/material"
 
 export function ProductTableFooter(props) {
 
@@ -9,14 +9,16 @@ export function ProductTableFooter(props) {
     const onPageSizeChange = props.onPageSizeChange
 
     return (
-        <TablePagination
-            count={count}
-            page={page - 1}
-            rowsPerPage={pageSize}
-            onPageChange={(event, page) => onPageChange(page + 1)}
-            onRowsPerPageChange={e => onPageSizeChange(e.target.value)}
-            rowsPerPageOptions={[5, 10, 25, 50, 100]}
-        />
+        <TableRow>
+            <TablePagination
+                count={count}
+                page={page - 1}
+                rowsPerPage={pageSize}
+                onPageChange={(event, page) => onPageChange(page + 1)}
+                onRowsPerPageChange={e => onPageSizeChange(e.target.value)}
+                rowsPerPageOptions={[5, 10, 25, 50, 100]}
+            />
+        </TableRow>
     )
 
 }

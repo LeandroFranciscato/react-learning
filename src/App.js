b6cb099343c159a768f260b9182235ab0ee60ca1
Leandro Franciscato <leandro.franciscato@clevertech.biz>
2022-09-15 00:34:43 -0300

    removing node modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import './App.css';
 import { FilterableProductTable } from './components/FilterableProductTable';
 
 const queryClient = new QueryClient()
